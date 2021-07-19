efc3fc863c1f11a8cf4ecfa90409b1a521b594af
jccjd <1340944538@qq.com>
2021-07-19 06:13:05 -1000

    first commit

---
+++ hell.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Hello World"
+# system variables user space and system space 
+
+echo $BASH
+echo $BASH_VERSION
+
+# No Programming language is perfect. There is not even a single best language
+# there are only languages well suited or perhaps poorly suited for paricular purposes
+
+# 
+
