1a4269abb0aef3a95f0d8a01b1507af66c790c28
jccjd <1340944538@qq.com>
2021-07-19 13:55:27 -1000

    Put a url

--- hell.sh
+++ hell.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# https://tldp.org/LDP/abs/html/sha-bang.html code
 echo "Hello World"
 # system variables user space and system space 
 
@@ -8,5 +9,9 @@ echo $BASH_VERSION
 # No Programming language is perfect. There is not even a single best language
 # there are only languages well suited or perhaps poorly suited for paricular purposes
 
-# 
-
+# Cleanup
+# Run as root, of course.
+cd /var/log
+cat /dev/null > message
+cat /dev/null > wtmp
+echo "Log files cleaned up"
